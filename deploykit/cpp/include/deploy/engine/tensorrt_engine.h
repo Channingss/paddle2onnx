5880fcdcc92f8cbb275da99a480e2c976f260379
channingss <chen_lingchi@163.com>
2021-03-08 13:10:31 +0000

    add trt

--- deploykit/cpp/include/deploy/engine/tensorrt_engine.h
+++ deploykit/cpp/include/deploy/engine/tensorrt_engine.h
@@ -15,112 +15,101 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
-#include <memory>
+#include <map>
 
 #include "NvInfer.h"
 
 #include "include/deploy/common/blob.h"
-
-
-
-namespace nic = nvidia::inferenceserver::client;
+#include "include/deploy/engine/tensorrt/buffers.h"
 
 namespace Deploy {
 
-using InferUniquePtr = std::unique_ptr<T, InferDeleter>; 
+using InferUniquePtr = std::unique_ptr<T, InferDeleter>;
 using Severity = nvinfer1::ILogger::Severity;
 
-struct InferDeleter
-{
-    template <typename T>
-    void operator()(T* obj) const
-    {
-        if (obj)
-        {
-            obj->destroy();
-        }
+struct InferDeleter {
+  template <typename T> void operator()(T *obj) const {
+    if (obj) {
+      obj->destroy();
     }
+  }
 };
 
 struct TensorRTInferenceConfigs {
+  void AnalysisConfig::SetTRTDynamicShapeInfo(
+      std::map<std::string, std::vector<int>> min_input_shape,
+      std::map<std::string, std::vector<int>> max_input_shape,
+      std::map<std::string, std::vector<int>> optim_input_shape) {
+    min_input_shape_ = min_input_shape;
+    max_input_shape_ = max_input_shape;
+    optim_input_shape_ = optim_input_shape;
+  }
 
-void AnalysisConfig::SetTRTDynamicShapeInfo(
-    std::map<std::string, std::vector<int>> min_input_shape,
-    std::map<std::string, std::vector<int>> max_input_shape,
-    std::map<std::string, std::vector<int>> optim_input_shape) {
-  min_input_shape_ = min_input_shape;
-  max_input_shape_ = max_input_shape;
-  optim_input_shape_ = optim_input_shape;
-
-}
-
-  protected:
-   nvinfer1::ILogger& logger nvinfer1::ILogger& logger = NaiveLogger.Global();
-   std::map<std::string, std::vector<int>> min_input_shape_{};
-   std::map<std::string, std::vector<int>> max_input_shape_{};
-   std::map<std::string, std::vector<int>> optim_input_shape_{};
+ protected:
+  nvinfer1::ILogger &logger nvinfer1::ILogger &logger = NaiveLogger.Global();
+  std::map<std::string, std::vector<int>> min_input_shape_{};
+  std::map<std::string, std::vector<int>> max_input_shape_{};
+  std::map<std::string, std::vector<int>> optim_input_shape_{};
 };
 
 // A logger for create TensorRT infer builder.
 class NaiveLogger : public nvinfer1::ILogger {
  public:
-    NaiveLogger(Severity severity = Severity::kWARNING)
-        : mReportableSeverity(severity)
-    {
-    }
-  void log(nvinfer1::ILogger::Severity severity, const char* msg) override {
+  explicit NaiveLogger(Severity severity = Severity::kWARNING)
+      : mReportableSeverity(severity) {}
+
+  void log(nvinfer1::ILogger::Severity severity, const char *msg) override {
     switch (severity) {
-      case Severity::kINFO:
-        VLOG(3) << msg;
-        break;
-      case Severity::kWARNING:
-        LOG(WARNING) << msg;
-        break;
-      case Severity::kINTERNAL_ERROR:
-      case Severity::kERROR:
-        LOG(ERROR) << msg;
-        break;
-      default:
-        break;
+    case Severity::kINFO:
+      VLOG(3) << msg;
+      break;
+    case Severity::kWARNING:
+      LOG(WARNING) << msg;
+      break;
+    case Severity::kINTERNAL_ERROR:
+    case Severity::kERROR:
+      LOG(ERROR) << msg;
+      break;
+    default:
+      break;
     }
   }
 
-  static nvinfer1::ILogger& Global() {
-    static nvinfer1::ILogger* x = new NaiveLogger;
+  static nvinfer1::ILogger &Global() {
+    static nvinfer1::ILogger *x = new NaiveLogger;
     return *x;
   }
 
   ~NaiveLogger() override {}
 
  private:
-
   Severity mReportableSeverity;
 };
 
 class TensorRTInferenceEngine {
  public:
-  void Init(std::string model_dir, int max_workspace_size, int max_batch_size, std::string trt_cache_filebool="", TensorRTInferenceConfigs configs=TensorRTInferenceConfigs());
+  void Init(std::string model_dir, int max_workspace_size, int max_batch_size,
+            std::string trt_cache_filebool = "",
+            TensorRTInferenceConfigs configs = TensorRTInferenceConfigs());
 
   void Infer(const std::vector<DataBlob> &input_blobs,
-	     std::vector<DataBlob> *output_blobs);
+             std::vector<DataBlob> *output_blobs);
 
   std::unique_ptr<nvinfer1::ICudaEngine> engine_;
 
  private:
   void ParseConfigs(const TritonInferenceConfigs &configs,
-		    nic::InferOptions *options);
+                    nic::InferOptions *options);
 
-  void CreateInput(const std::vector<DataBlob> &input_blobs,
-		   std::vector<nic::InferInput *> *inputs);
+  void FeedInput(const std::vector<DataBlob> &input_blobs,
+                 std::vector<nic::InferInput *> *inputs);
 
-  void CreateOutput(const rapidjson::Document &model_metadata,
-		    std::vector<const nic::InferRequestedOutput *> *outputs);
+  void FetchOutput(c std::vector<const nic::InferRequestedOutput *> *outputs);
 
   void ParseONNXModel(const std::string &model_dir);
-
 };
 
-}  // namespace Deploy
-
+}  //  namespace Deploy
--- deploykit/cpp/src/engine/tensorrt_engine.cpp
+++ deploykit/cpp/src/engine/tensorrt_engine.cpp
@@ -16,23 +16,48 @@
 
 namespace Deploy {
 
+void TensorRTInferenceEngine::Init(std::string model_dir,
+        uint32_t max_workspace_size,
+        uint32_t max_batch_size,
+        std::string trt_cache_filebool = "",
+        TensorRTInferenceConfigs configs) {
+  auto builder = InferUniquePtr<nvinfer1::IBuilder>(
+      nvinfer1::createInferBuilder(configs.logger_));
+  auto config =
+      InferUniquePtr<nvinfer1::IBuilderConfig>(builder->createBuilderConfig());
 
-void TensorRTInferenceEngine::Init(std::string model_dir, uint32_t max_workspace_size, uint32_t max_batch_size, std::string trt_cache_filebool="", TensorRTInferenceConfigs configs}) {
-  auto builder = InferUniquePtr<nvinfer1::IBuilder>(nvinfer1::createInferBuilder(configs.logger_));
-  
-  const auto explicitBatch = max_bath_size << static_cast<uint32_t>(NetworkDefinitionCreationFlag::kEXPLICIT_BATCH);
-  auto network = InferUniquePtr<nvinfer1::INetworkDefinition>(builder->createNetworkV2(explicitBatch));
-  auto config = InferUniquePtr<nvinfer1::IBuilderConfig>(builder->createBuilderConfig());
-  auto parser = InferUniquePtr<nvonnxparser::IParser>(nvonnxparser::createParser(*network, configs.logger_));
-  auto parsed = parser->parseFromFile(model_dir.c_str(), static_cast<int>(logger.mReportableSeverity)); 
-  config->setMaxWorkspaceSize(max_workspace_size)
-  engine_ = std::shared_ptr<nvinfer1::ICudaEngine>(
-        builder->buildEngineWithConfig(*network, *config), InferDeleter()); 
+  auto profile = builder->createOptimizationProfile();
+  profile->setDimensions(input->getName(), OptProfileSelector::kMIN,
+                         Dims4{calibBatchSize, 1, 28, 28});
+  profile->setDimensions(input->getName(), OptProfileSelector::kOPT,
+                         Dims4{calibBatchSize, 1, 28, 28});
+  profile->setDimensions(input->getName(), OptProfileSelector::kMAX,
+                         Dims4{calibBatchSize, 1, 28, 28});
+  config->addOptimizationProfile(profile);
+
+  const auto explicitBatch =
+      max_bath_size << static_cast<uint32_t>(
+          NetworkDefinitionCreationFlag::kEXPLICIT_BATCH);
+  auto network = InferUniquePtr<nvinfer1::INetworkDefinition>(
+      builder->createNetworkV2(explicitBatch));
+
+  auto parser = InferUniquePtr<nvonnxparser::IParser>(
+      nvonnxparser::createParser(*network, configs.logger_));
+  auto parsed = parser->parseFromFile(
+      model_dir.c_str(), static_cast<int>(logger.mReportableSeverity));
+  config->setMaxWorkspaceSize(max_workspace_size) engine_ =
+      std::shared_ptr<nvinfer1::ICudaEngine>(
+          builder->buildEngineWithConfig(*network, *config), InferDeleter());
 }
 
-  void TensorRTInferenceEngine::Infer(const std::vector<DataBlob> &input_blobs,
-	     std::vector<DataBlob> *output_blobs){
-  
-  
-  }
+void TensorRTInferenceEngine::Infer(const std::vector<DataBlob> &input_blobs,
+                                    const int batch_size,
+                                    std::vector<DataBlob> *output_blobs) {
+  auto context = InferUniquePtr<nvinfer1::IExecutionContext>(
+      engine_->createExecutionContext());
+  TensorRT::BufferManager buffers(engine, batch_size, context);
+  ProcessInput(input_blobs, buffers) buffers.copyInputToDevice();
+  bool status = context->executeV2(buffers.getDeviceBindings().data());
+  buffers.copyOutputToHost();
+}
 }  //  namespace Deploy
